4650d46058d421a9c73a1289af43d5028e731891
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2025-03-03 07:43:21 -0500

    Update model.py

--- model.py
+++ model.py
@@ -162,8 +162,8 @@ class GPTConfig:
     dropout: float = 0.0
     bias: bool = True  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
     return_features: bool = False  # New flag to return hidden representations
-    self.use_rope: bool = True
-    self.noise_alpha: float = 0.5
+    use_rope: bool = True
+    noise_alpha: float = 0.5
 
 class GPT(nn.Module):
 
