d6d203286d856cd0a8d60c18ce803f06b49ae53d
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2025-03-03 08:24:54 -0500

    Update model.py

--- model.py
+++ model.py
@@ -126,14 +126,24 @@ class MLP(nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        self.c_fc3 = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.c_fc2 = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
         self.gelu    = RainstarActivation()
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=False) #KAN style
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
-        x = self.c_fc(x)
-        x = self.gelu(x)
+        a = self.c_fc(x)
+        b = self.c_fc2(x)    
+        c = self.c_fc3(x)
+        a = torch.sigmoid(a)
+        b = torch.sigmoid(b)
+        signal = 0.5 * (a + b - 2.0 * a * b)
+        c = c * signal #apply xor gaing
+        x = self.gelu(c)
+        
         x = self.c_proj(x)
         x = self.dropout(x)
         return x
