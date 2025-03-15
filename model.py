81ba2f25370815cc78782c27508c9eb412f179c6
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2025-03-15 15:41:42 -0400

    Update model.py

--- model.py
+++ model.py
@@ -313,7 +313,7 @@ class GPT(nn.Module):
         
         for i, block in enumerate(self.transformer.residual):
             #for all but the first and last process througn an interblock from self.transformer.secondary
-            if i %2 and i < len(self.transformer.residual)):
+            if i %2 and i < len(self.transformer.residual):
                  self.transformer.residual[i].attn.c_attn.weight = (
                         self.transformer.residual[i-1].attn.c_attn.weight +
                         self.transformer.residual[i + 1].attn.c_attn.weight
